fa520624a76533e16eb71aeddc60798f02a3fb2c
Sam Nystrom <samuel.l.nystrom@gmail.com>
2022-08-04 10:13:43 -0400

    Add Roadrunner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.kt
@@ -0,0 +1,298 @@
+package org.firstinspires.ftc.teamcode.drive
+
+import com.acmerobotics.dashboard.config.Config
+import com.acmerobotics.roadrunner.control.PIDCoefficients
+import com.acmerobotics.roadrunner.drive.MecanumDrive
+import com.acmerobotics.roadrunner.followers.HolonomicPIDVAFollower
+import com.acmerobotics.roadrunner.followers.TrajectoryFollower
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.acmerobotics.roadrunner.trajectory.Trajectory
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder
+import com.acmerobotics.roadrunner.trajectory.constraints.*
+
+import com.qualcomm.hardware.bosch.BNO055IMU
+import com.qualcomm.hardware.lynx.LynxModule
+import com.qualcomm.robotcore.hardware.*
+
+import org.firstinspires.ftc.teamcode.drive.DriveConstants.encoderTicksToInches
+import org.firstinspires.ftc.teamcode.drive.DriveConstants.kV
+import org.firstinspires.ftc.teamcode.drive.DriveConstants.kA
+import org.firstinspires.ftc.teamcode.drive.DriveConstants.kStatic
+import org.firstinspires.ftc.teamcode.drive.DriveConstants.TRACK_WIDTH
+import org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ANG_ACCEL
+import org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ANG_VEL
+import org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_VEL
+import org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ACCEL
+import org.firstinspires.ftc.teamcode.drive.DriveConstants.MOTOR_VELO_PID
+import org.firstinspires.ftc.teamcode.drive.DriveConstants.RUN_USING_ENCODER
+
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceRunner
+
+import org.firstinspires.ftc.teamcode.util.LynxModuleUtil
+
+import kotlin.math.abs
+
+/*
+* Simple mecanum drive hardware implementation for REV hardware.
+*/
+@Config
+class SampleMecanumDrive(hardwareMap: HardwareMap) :
+    MecanumDrive(kV, kA, kStatic, TRACK_WIDTH, TRACK_WIDTH, LATERAL_MULTIPLIER) {
+    private val trajectorySequenceRunner: TrajectorySequenceRunner
+    private val follower: TrajectoryFollower
+    private val leftFront: DcMotorEx
+    private val leftRear: DcMotorEx
+    private val rightRear: DcMotorEx
+    private val rightFront: DcMotorEx
+    private val motors: List<DcMotorEx>
+    private val imu: BNO055IMU
+    private val batteryVoltageSensor: VoltageSensor
+
+    fun trajectoryBuilder(startPose: Pose2d): TrajectoryBuilder {
+        return TrajectoryBuilder(
+            startPose=startPose,
+            baseVelConstraint=VEL_CONSTRAINT,
+            baseAccelConstraint=ACCEL_CONSTRAINT
+        )
+    }
+
+    fun trajectoryBuilder(startPose: Pose2d?, reversed: Boolean): TrajectoryBuilder {
+        return TrajectoryBuilder(startPose!!, reversed, VEL_CONSTRAINT, ACCEL_CONSTRAINT)
+    }
+
+    fun trajectoryBuilder(startPose: Pose2d?, startHeading: Double): TrajectoryBuilder {
+        return TrajectoryBuilder(startPose!!, startHeading, VEL_CONSTRAINT, ACCEL_CONSTRAINT)
+    }
+
+    fun trajectorySequenceBuilder(startPose: Pose2d?): TrajectorySequenceBuilder {
+        return TrajectorySequenceBuilder(
+            startPose,
+            VEL_CONSTRAINT, ACCEL_CONSTRAINT,
+            MAX_ANG_VEL, MAX_ANG_ACCEL
+        )
+    }
+
+    fun turnAsync(angle: Double) {
+        trajectorySequenceRunner.followTrajectorySequenceAsync(
+            trajectorySequenceBuilder(poseEstimate)
+                .turn(angle)
+                .build()
+        )
+    }
+
+    fun turn(angle: Double) {
+        turnAsync(angle)
+        waitForIdle()
+    }
+
+    fun followTrajectoryAsync(trajectory: Trajectory) {
+        trajectorySequenceRunner.followTrajectorySequenceAsync(
+            trajectorySequenceBuilder(trajectory.start())
+                .addTrajectory(trajectory)
+                .build()
+        )
+    }
+
+    fun followTrajectory(trajectory: Trajectory) {
+        followTrajectoryAsync(trajectory)
+        waitForIdle()
+    }
+
+    fun followTrajectorySequenceAsync(trajectorySequence: TrajectorySequence?) {
+        trajectorySequenceRunner.followTrajectorySequenceAsync(trajectorySequence)
+    }
+
+    fun followTrajectorySequence(trajectorySequence: TrajectorySequence?) {
+        followTrajectorySequenceAsync(trajectorySequence)
+        waitForIdle()
+    }
+
+    val lastError: Pose2d
+        get() = trajectorySequenceRunner.lastPoseError
+
+    fun update() {
+        updatePoseEstimate()
+        val signal = trajectorySequenceRunner.update(poseEstimate, poseVelocity)
+        signal?.let { setDriveSignal(it) }
+    }
+
+    fun waitForIdle() {
+        while (!Thread.currentThread().isInterrupted && isBusy) update()
+    }
+
+    val isBusy: Boolean
+        get() = trajectorySequenceRunner.isBusy
+
+    fun setMode(runMode: DcMotor.RunMode?) {
+        for (motor in motors) {
+            motor.mode = runMode
+        }
+    }
+
+    fun setZeroPowerBehavior(zeroPowerBehavior: DcMotor.ZeroPowerBehavior?) {
+        for (motor in motors) {
+            motor.zeroPowerBehavior = zeroPowerBehavior
+        }
+    }
+
+    fun setPIDFCoefficients(runMode: DcMotor.RunMode?, coefficients: PIDFCoefficients) {
+        val compensatedCoefficients = PIDFCoefficients(
+            coefficients.p, coefficients.i, coefficients.d,
+            coefficients.f * 12 / batteryVoltageSensor.voltage
+        )
+        for (motor in motors) {
+            motor.setPIDFCoefficients(runMode, compensatedCoefficients)
+        }
+    }
+
+    fun setWeightedDrivePower(drivePower: Pose2d) {
+        var vel = drivePower
+        if ((abs(drivePower.x) + abs(drivePower.y)
+                    + abs(drivePower.heading)) > 1
+        ) {
+            // re-normalize the powers according to the weights
+            val denom =
+                VX_WEIGHT * abs(drivePower.x) + VY_WEIGHT * abs(drivePower.y) + OMEGA_WEIGHT * abs(
+                    drivePower.heading
+                )
+            vel = Pose2d(
+                VX_WEIGHT * drivePower.x,
+                VY_WEIGHT * drivePower.y,
+                OMEGA_WEIGHT * drivePower.heading
+            ).div(denom)
+        }
+        setDrivePower(vel)
+    }
+
+    override fun getWheelPositions(): List<Double> {
+        val wheelPositions: MutableList<Double> = ArrayList()
+        for (motor in motors) {
+            wheelPositions.add(encoderTicksToInches(motor.currentPosition.toDouble()))
+        }
+        return wheelPositions
+    }
+
+    override fun getWheelVelocities(): List<Double>? {
+        val wheelVelocities: MutableList<Double> = ArrayList()
+        for (motor in motors) {
+            wheelVelocities.add(encoderTicksToInches(motor.velocity))
+        }
+        return wheelVelocities
+    }
+
+    override fun setMotorPowers(
+        frontLeft: Double,
+        rearLeft: Double,
+        rearRight: Double,
+        frontRight: Double,
+    ) {
+        leftFront.power = frontLeft
+        leftRear.power = rearLeft
+        rightRear.power = rearRight
+        rightFront.power = frontRight
+    }
+
+    override val rawExternalHeading: Double
+        get() = imu.angularOrientation.firstAngle.toDouble()
+
+    override fun getExternalHeadingVelocity(): Double? {
+        // To work around an SDK bug, use -zRotationRate in place of xRotationRate
+        // and -xRotationRate in place of zRotationRate (yRotationRate behaves as
+        // expected). This bug does NOT affect orientation.
+        //
+        // See https://github.com/FIRST-Tech-Challenge/FtcRobotController/issues/251 for details.
+        return -imu.angularVelocity.xRotationRate.toDouble()
+    }
+
+    companion object {
+        var TRANSLATIONAL_PID = PIDCoefficients(0.0, 0.0, 0.0)
+        var HEADING_PID = PIDCoefficients(0.0, 0.0, 0.0)
+        var LATERAL_MULTIPLIER = 1.0
+        var VX_WEIGHT = 1.0
+        var VY_WEIGHT = 1.0
+        var OMEGA_WEIGHT = 1.0
+        private val VEL_CONSTRAINT = getVelocityConstraint(MAX_VEL, MAX_ANG_VEL, TRACK_WIDTH)
+        private val ACCEL_CONSTRAINT = getAccelerationConstraint(MAX_ACCEL)
+        fun getVelocityConstraint(
+            maxVel: Double,
+            maxAngularVel: Double,
+            trackWidth: Double
+        ): TrajectoryVelocityConstraint {
+            return MinVelocityConstraint(
+                listOf(
+                    AngularVelocityConstraint(maxAngularVel),
+                    MecanumVelocityConstraint(maxVel, trackWidth)
+                )
+            )
+        }
+
+        fun getAccelerationConstraint(maxAccel: Double): TrajectoryAccelerationConstraint {
+            return ProfileAccelerationConstraint(maxAccel)
+        }
+    }
+
+    init {
+        follower = HolonomicPIDVAFollower(
+            TRANSLATIONAL_PID, TRANSLATIONAL_PID, HEADING_PID,
+            Pose2d(0.5, 0.5, Math.toRadians(5.0)), 0.5
+        )
+        LynxModuleUtil.ensureMinimumFirmwareVersion(hardwareMap)
+        batteryVoltageSensor = hardwareMap.voltageSensor.iterator().next()
+        for (module in hardwareMap.getAll(LynxModule::class.java)) {
+            module.bulkCachingMode = LynxModule.BulkCachingMode.AUTO
+        }
+
+        // TODO: adjust the names of the following hardware devices to match your configuration
+        imu = hardwareMap.get(BNO055IMU::class.java, "imu")
+        val parameters = BNO055IMU.Parameters()
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS
+        imu.initialize(parameters)
+
+        // TODO: If the hub containing the IMU you are using is mounted so that the "REV" logo does
+        // not face up, remap the IMU axes so that the z-axis points upward (normal to the floor.)
+        //
+        //             | +Z axis
+        //             |
+        //             |
+        //             |
+        //      _______|_____________     +Y axis
+        //     /       |_____________/|__________
+        //    /   REV / EXPANSION   //
+        //   /       / HUB         //
+        //  /_______/_____________//
+        // |_______/_____________|/
+        //        /
+        //       / +X axis
+        //
+        // This diagram is derived from the axes in section 3.4 https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bno055-ds000.pdf
+        // and the placement of the dot/orientation from https://docs.revrobotics.com/rev-control-system/control-system-overview/dimensions#imu-location
+        //
+        // For example, if +Y in this diagram faces downwards, you would use AxisDirection.NEG_Y.
+        // BNO055IMUUtil.remapZAxis(imu, AxisDirection.NEG_Y);
+        leftFront = hardwareMap.get(DcMotorEx::class.java, "leftFront")
+        leftRear = hardwareMap.get(DcMotorEx::class.java, "leftRear")
+        rightRear = hardwareMap.get(DcMotorEx::class.java, "rightRear")
+        rightFront = hardwareMap.get(DcMotorEx::class.java, "rightFront")
+        motors = listOf(leftFront, leftRear, rightRear, rightFront)
+        for (motor in motors) {
+            val motorConfigurationType = motor.motorType.clone()
+            motorConfigurationType.achieveableMaxRPMFraction = 1.0
+            motor.motorType = motorConfigurationType
+        }
+        if (RUN_USING_ENCODER) {
+            setMode(DcMotor.RunMode.RUN_USING_ENCODER)
+        }
+        setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+        if (RUN_USING_ENCODER && MOTOR_VELO_PID != null) {
+            setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, MOTOR_VELO_PID)
+        }
+
+        // TODO: reverse any motors using DcMotor.setDirection()
+
+        // TODO: if desired, use setLocalizer() to change the localization method
+        // for instance, setLocalizer(new ThreeTrackingWheelLocalizer(...));
+        trajectorySequenceRunner = TrajectorySequenceRunner(follower, HEADING_PID)
+    }
+}
